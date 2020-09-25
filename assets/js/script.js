aa7bbe4eda7e654a4553b22f375f8bbe9ecfa992
Tessa Pierce <tesssapierce@gmail.com>
2020-09-24 18:15:47 -0600

    Updating JS file to show password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numerical = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
-// When use clicks on button, create random assignment
+// When use clicks on button, create random password
 // When user clicks, prompt for how long password should be
 //   -- What if the character count is outside of 8-128 : Alert
 // When user clicks, confirm if user wants LC, UC, NO, SP
@@ -20,14 +21,19 @@ var special = ["!", "@", "#", "$", "%", "^", "&", "*"];
 // Functions
 
 function getPasswordOptions(){
-  var length = parseInt(prompt("How many characters would you like?"))
-  if (length < 8) {
+  var confirmLength = parseInt(prompt("How many characters would you like?"))
+  if (confirmLength < 8) {
     alert("Please enter a number between 8-128")
     return
-  } if (length > 128) {
+  } if (confirmLength > 128) {
     alert("Please enter a number between 8-128")
     return
-  } // TODO: Validate if it is a number
+  } if (Number.isInteger(confirmLength) === false) {
+    alert("Please enter a number between 8-128")
+    return
+  }
+
+  console.log(confirmLength)
 
   // Confirm what characters user wants
   var lcConfirm = confirm("Do you want lowercase letters?")
@@ -35,21 +41,21 @@ function getPasswordOptions(){
   var nConfirm = confirm("Do you want numbers?")
   var sConfirm = confirm("Do you want special characters?")
 
-  // TODO: Might have to switch this to lcConfirm === false && etc
+  // If doesn't confirm any, end
   if (!lcConfirm && !ucConfirm && !nConfirm && !sConfirm) {
     alert("Hey man please pick something")
     return
   }
 
-  // Object of true/false
   var passwordOptions = {
-    length: length,
+    confirmLength: confirmLength,
     lcConfirm: lcConfirm,
     ucConfirm: ucConfirm,
     nConfirm: nConfirm,
     sConfirm: sConfirm,
   }
-  console.log("Password Options: " + passwordOptions);
+
+  console.log("Password Options" + passwordOptions);
   return passwordOptions;
 }
 
@@ -61,23 +67,39 @@ function randomizer(arr){
   return randomElement
 }
 
-//options contains the object above
+// Creates the password
 function generatePassword(){
   var options = getPasswordOptions();
-  var result = []
-  var possibleChar = []
-  var guaranteedChar = []
+  var result = [];
+  var possibleChar = [];
+  var guaranteedChar = [];
 
-  if (options.lcConfirm) {
-    possibleChar = possibleChar.concat(lowercase);
-    guaranteedChar.push(randomizer(lowercase));
-  } if (options.ucConfirm) {
-    possibleChar = possibleChar.concat(uppercase);
-    guaranteedChar.push(randomizer(uppercase));
-  } // TODO: finish this
+    if (options.lcConfirm) {
+      possibleChar = possibleChar.concat(lowercase);
+      guaranteedChar.push(randomizer(lowercase));
+    } if (options.ucConfirm) {
+      possibleChar = possibleChar.concat(uppercase);
+      guaranteedChar.push(randomizer(uppercase));
+    } if (options.nConfirm) {
+      possibleChar = possibleChar.concat(numerical);
+      guaranteedChar.push(randomizer(numerical));
+    } if (options.ucConfirm) {
+      possibleChar = possibleChar.concat(special);
+      guaranteedChar.push(randomizer(special));
+    }
 
-  console.log(result);
-  return result;
+  for (var i=0; i < options.confirmLength; i++){
+    result.push(randomizer(possibleChar));
+  }
+
+  for (var i=0; i < guaranteedChar.length; i++){
+    result[i] = guaranteedChar[i]
+  }
+
+  console.log(guaranteedChar);
+  console.log(possibleChar);
+  console.log(result)
+  return result.join("");
 }
 
 // Write password to the #password input
